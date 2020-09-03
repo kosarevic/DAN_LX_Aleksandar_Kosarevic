4e174eeb23304cc0c92e0d5ac7280f01550f3d33
Kosarevic <kosarevic@gmail.com>
2020-09-03 18:23:36 +0200

    Implemented Model classes

---
+++ Zadatak_1/Model/Location.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class Location
+    {
+        public int Id { get; set; }
+        public string Adress { get; set; }
+        public string Town { get; set; }
+        public string Country { get; set; }
+
+    }
+}
---
+++ Zadatak_1/Model/MainWindowModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1.Model
+{
+    class MainWindowModel
+    {
+
+        public Employee Employee { get; set; }
+        public Location Location { get; set; }
+        public Sector Sector { get; set; }
+        public int ManagerId { get; set; }
+
+    }
+}
